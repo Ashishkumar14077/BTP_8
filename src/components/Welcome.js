ba13cfbc83d7cd6c06cc5aeb6e012e8faa69e325
Ashishkumar14077 <pintubrajkishor@gmail.com>
2023-04-26 15:16:47 +0530

    added welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,6 @@ import React from "react";
 
 function Welcome({ account, setAccount }) {
   return (
-
     <div className="home">
       {/* <h2>Welcome to Realestate</h2> */}
       <div className="container-fluid mt-5">
@@ -15,22 +14,22 @@ function Welcome({ account, setAccount }) {
                 </div>
               </div>
             ) : (
-              <div className="pagi">
-                Hello u have logged in
-
+              <div className="img-slider">
+                <h1 className="input-wrapper">
+                  Welcome to the RealEstate Marketplace
+                </h1>
+                <div class="slider-container">
+                  <div class="slide">
+                    <img src="https://cdn.luxe.digital/media/20230123162705/most-expensive-houses-in-the-world-reviews-luxe-digital-1560x780.jpg.webp" />
+                  </div>
+                  <div class="slide">
+                    <img src="https://img.uswitch.com/t014ej9w3ur1/1OYYdq7OHEx0Ho8StLGwq8/b3fb654a7b9f80a06a58912012651523/shutterstock_593346773.jpg?auto=format%2Ccompress&q=45&ixlib=react-9.5.1-beta.1&w=1476" />
+                  </div>
+                  <div class="slide">
+                    <img src="https://hips.hearstapps.com/hmg-prod/images/beach-house-lr-to-ocean-1556640354.jpg?crop=1.00xw:0.334xh;0,0.399xh&resize=1200:*" />
+                  </div>
+                </div>
               </div>
-              // <div className="pagi">
-              // <base href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/"/>
-              // <div id="slider">
-              // <figure>
-              // <img src="austin-fireworks.jpg" alt/> 
-              // <img src="taj-mahal_copy.jpg" alt/>
-              // <img src="ibiza.jpg" alt/>
-              // <img src="ankor-wat.jpg" alt/>
-              // <img src="austin-fireworks.jpg" alt/>
-              // </figure>
-              // </div>
-              // </div>
             )}
           </main>
         </div>
